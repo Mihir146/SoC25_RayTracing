4379582193b53a88127c4830067f10e5927f2db3
Mihir <mihirv416@gmail.com>
2025-05-28 19:54:23 +0530

    Created vec3 Class

--- week_1/vec3.h
+++ week_1/vec3.h
@@ -1,9 +1,65 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include<cmath>
+#include<iostream>
+
 class vec3{
    public:
-       double e[3];
-       
-}
+       double e[3]; //array to store vector components
+
+       //member initializer lists 
+
+       vec3()   
+           : e{0,0,0} {}
+
+       vec3(double e0, double e1,double e2) 
+           : e{e0, e1, e2} {}
+
+       //member functions to get the coordinates of vector: 
+
+       double x() const { return e[0]; }
+       double y() const { return e[1]; }
+       double z() const { return e[2]; }
+
+       //operators:
+       vec3 operator-() const{
+           return vec3(-e[0], -e[1], -e[2]);
+       }
+       double operator[](int i) const{
+           return e[i];
+       }
+       double& operator[](int i){
+           return e[i];
+       }
+
+       vec3& operator+=(const vec3& v) {
+           e[0] += v.e[0];
+           e[1] += v.e[1];
+           e[2] += v.e[2];
+           return *this;
+       }
+        
+       vec3& operator*=(double t) {
+           e[0] *= t;
+           e[1] *= t;
+           e[2] *= t;
+           return *this;
+       }
+
+       vec3& operator/=(double t) {
+           return *this *= 1/t;
+       }
+
+       //some useful member functions:
+       double length_squared(){
+           return (e[0]*e[0]+e[1]*e[1]+e[2]*e[2]);
+       }
+
+       double length(){
+           return std::sqrt(length_squared());
+       }
+};
+
+
 #endif
