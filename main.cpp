c36f1acec3e8653e27240328f44c6c59bc61b30a
Mihir <mihirv416@gmail.com>
2025-06-10 19:54:22 +0530

    create a sphere

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 #include"color.h"
 #include"ray.h"
 
+//this fn will check if the given ray hits the given sphere
+double hit_sphere(const point3& center, const double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = r.direction().length_squared();
+    auto h = dot(r.direction(), oc);
+    auto c = oc.length_squared() - radius*radius;
+    auto discriminant = h*h - a*c;
+    if(discriminant < 0.0){
+        return -1.0;
+    }
+    else{
+        //not considering the +D term cuz it would give the second point(behind) the ray hits
+        return (h-std::sqrt(discriminant))/a;
+    }
+}
+
 color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1), 0.5, r);
+    if(t>0.0){
+        //N is the surface normal vector (normalized)
+        vec3 N = (r.at(t) - vec3(0,0,-1)).unit(); 
+        //we are mapping the unit surface normal vectors components from [-1,1] to [0,1] to be used as a "color vector"
+        return 0.5*color(N.x()+1.0,N.y()+1.0,N.z()+1.0);
+    }
     vec3 unit_direction = r.direction().unit();
     //a should be b/w 0 and 1 and y comp of unit_direction is b/w -1 and 1 so we making sure to adjust it 
     auto a = 0.5*(unit_direction.y() + 1.0);
