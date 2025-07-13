9a63bddecbc365a8ad5a99b501e1c16576596921
Mihir Vasave <mihirv416@gmail.com>
2025-07-13 22:45:31 +0530

    implemented antialiasing

--- interval.h
+++ interval.h
@@ -18,6 +18,14 @@ class interval{
         bool surrounds(double x) const{
             return min < x && x < max;
         }
+
+        //to take care of x if it is outside of min max range
+        double clamp(double x) const{
+            if(x<min) return min;
+            if(x>max) return max;
+            return x;
+        }
+
         static const interval empty, universe;
 };
 
