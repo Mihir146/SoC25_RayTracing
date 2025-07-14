6f6e821423141085cc3c87f59a2987ddf1f1539a
Mihir Vasave <mihirv416@gmail.com>
2025-07-15 00:25:52 +0530

    added gamma correction

--- color.h
+++ color.h
@@ -6,11 +6,23 @@
 //#include<iostream>
 using color = vec3;
 
+inline double linear_to_gamma(double linear_component)
+{
+    if (linear_component > 0)
+        return std::sqrt(linear_component);
+
+    return 0;
+}
 //pixel_color vector should have components in the range [0,1] only 
 void write_color(std::ostream& out, const color& pixel_color){
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
